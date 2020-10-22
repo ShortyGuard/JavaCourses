807e8d04fc0d2461d30ad16396bc99548ba6851b
Oleg.Orlov <shortyguard@mail.ru>
2020-10-22 21:19:14 +0700

    Выполнено домашнее задание урока 3 Курса Java. Уровень 1 (поправлены замечания)

--- Java1/src/main/java/lesson3/HomeWorkLesson3.java
+++ Java1/src/main/java/lesson3/HomeWorkLesson3.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HomeWorkLesson3 {
-    private Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
